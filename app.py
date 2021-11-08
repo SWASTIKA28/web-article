c1c3d3740db79c16bb2022bcc4a12c4b29e3f116
Swastik Mishra <swastikm2000@gmail.com>
2021-11-09 01:10:51 +0530

    Templates and wsgi.py files added

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+#model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -16,9 +16,9 @@ def predict():
     '''
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    #prediction = model.predict(final_features)
 
-    output = round(prediction[0], 2)
+    #output = round(prediction[0], 2)
 
     return render_template('index.html', prediction_text='Sentiment Analysis of Quote $ {}'.format(output))
 
@@ -28,9 +28,9 @@ def predict_api():
     For direct API calls trought request
     '''
     data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
+    #prediction = model.predict([np.array(list(data.values()))])
 
-    output = prediction[0]
+    #output = prediction[0]
     return jsonify(output)
 
 if __name__ == "__main__":
